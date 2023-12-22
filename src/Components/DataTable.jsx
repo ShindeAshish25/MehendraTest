bf1f22c3e5b753a9c4f66e069c82104c528b7cc5
ShindeAshish25 <114564001+ShindeAshish25@users.noreply.github.com>
2023-12-22 17:57:32 +0530

    Update DataTable.jsx

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,25 +6,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import Grid from '@mui/material/Grid';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs from 'dayjs';
-
-import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 import Swal from 'sweetalert2'
-
-
 
 function createData(ID, Date, Branch, Type, Amount, Bank, Requested, Status, Action) {
     return { ID, Date, Branch, Type, Amount, Bank, Requested, Status, Action };
@@ -32,82 +24,72 @@ function createData(ID, Date, Branch, Type, Amount, Bank, Requested, Status, Act
 
 const rows = [
     createData(124235, '19/04/2023', 'Thane', 'Full', '547890', 'CMV HDFC 1223562', 'Sharad Varma', 'Pending'),
-    createData(563233, '20/12/2023', 'Navi Mumbai', 'Full', '246732', 'UYT SCB 632786433', 'Pramod Mehata', 'Approved'),
-    createData(213352, '21/10/2023', 'Mumbai', 'Short', '553672', 'OIT HDFC 6732647333', 'Vikas Singh', 'rejected'),
-    createData(565223, '22/09/2023', 'Kurla', 'Full', '197146', 'YTF SBI 654426545', 'Shard Shivastav', 'Approved'),
-    createData(754833, '23/08/2023', 'Vile Parel', 'Full', '242178', 'PHS SBI 46465416', 'Vikas Mehata', 'Approved'),
-    createData(367323, '24/07/2023', 'Lower Parel', 'Short', '643211', 'PDS HDFC 96233321', 'Shard kapoor', 'rejected'),
-    createData(748333, '25/06/2023', 'Andheri', 'Full', '842789', 'GBG HDFC 3548225', 'Pramod Mahtur', 'Approved'),
-    createData(367324, '26/05/2023', 'Byculls', 'Full', '642843', 'MGB SCB 8984252', 'Vikas Shethi', 'Approved'),
+    createData(563233, '20/06/2023', 'Navi Mumbai', 'Full', '246732', 'UYT SCB 632786433', 'Pramod Mehata', 'Approved'),
+    createData(213352, '22/07/2023', 'Mumbai', 'Short', '553672', 'OIT HDFC 6732647333', 'Vikas Singh', 'rejected'),
+    createData(565223, '19/08/2023', 'Kurla', 'Full', '197146', 'YTF SBI 654426545', 'Shard Shivastav', 'Approved'),
+    createData(754833, '11/04/2023', 'Vile parle', 'Full', '242178', 'PHS SBI 46465416', 'Vikas Mehata', 'Approved'),
+    createData(367323, '15/03/2023', 'Lower Parel', 'Short', '643211', 'PDS HDFC 96233321', 'Shard kapoor', 'rejected'),
+    createData(748333, '17/08/2023', 'Andheri', 'Full', '842789', 'GBG HDFC 3548225', 'Pramod Mahtur', 'Approved'),
+    createData(367324, '12/09/2023', 'Byculla', 'Full', '642843', 'MGB SCB 8984252', 'Vikas Shethi', 'Approved'),
 ];
 
-export default function AccessibleTable() {
+function AdminPanel() {
+    const [fromDate, setFromDate] = useState(null);
+    const [toDate, setToDate] = useState(null);
+    const [branch, setBranch] = useState('');
+    const [type, setType] = useState('');
+    const [status, setStatus] = useState('');
+    const [filteredRows, setFilteredRows] = useState(rows);
+    const [data, setData] = useState(rows);
 
+    // const date = new Date('2017-4-28');
+    // const start = new Date(fromDate);
+    // const end = new Date(toDate);
 
-    const [fromDate, setFromDate] = React.useState(null);
-    const [toDate, setToDate] = React.useState(null);
-    const [branch, setBranch] = React.useState('');
-    const [type, setType] = React.useState('');
-    const [status, setStatus] = React.useState('');
-    const [filteredRows, setFilteredRows] = React.useState(rows);
+    // if (date > start && date < end) {
+    //     console.log('in between');
+    // } else {
+    //     console.log('outside');
+    // }
+    console.log(data);
 
     const handleDateChange = (date, type) => {
         if (type === 'from') {
-            setFromDate(date);
+            console.log(date);
+            setFromDate(dateFormater(date.$d));
         } else {
-            // Ensure "To" date is not less than "From" date
-            if (fromDate && date.isBefore(dayjs(fromDate), 'day')) {
+            if (fromDate && date < fromDate) {
                 return;
             }
-            setToDate(date);
+            setToDate(dateFormater(date.$d));
         }
     };
 
-
-    const handleBranchChange = (event) => {
-        setBranch(event.target.value);
+    const handleBranchChange = (e) => {
+        setBranch(e.target.value);
     };
 
-    const handleTypeChange = (event) => {
-        setType(event.target.value);
+    const handleTypeChange = (e) => {
+        setType(e.target.value);
     };
 
-    const handleStatusChange = (event) => {
-        setStatus(event.target.value);
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value);
     };
 
-    React.useEffect(() => {
-        // Apply filters to rows based on the selected options
-        const filtered = rows.filter((row) => {
-            const date = new Date(row.Date.split('/').reverse().join('/'));
-            const fromDateMatch = !fromDate || date >= fromDate;
-            const toDateMatch = !toDate || date <= toDate;
-            const branchMatch = !branch || row.Branch.toLowerCase() === branch.toLowerCase();
-            const typeMatch = !type || row.Type.toLowerCase() === type.toLowerCase();
-            const statusMatch = !status || row.Status.toLowerCase() === status.toLowerCase();
+    
+        const isDateInRange = (dateToCheck, startDate, endDate) => {
+            const checkDate = new Date(dateToCheck);
+            const start = new Date(startDate);
+            const end = new Date(endDate);
 
-            return fromDateMatch && toDateMatch && branchMatch && typeMatch && statusMatch;
-        });
+            return checkDate >= start && checkDate <= end;
+        };
+        const currentDate = new Date();
+        const startDate = '2020-08-01'; // Replace with your start date
+        const endDate = '2020-08-31';   // Replace with your end date
 
-        setFilteredRows(filtered);
-    }, [fromDate, toDate, branch, type, status]);
-
-
-    React.useEffect(() => {
-        // Apply filters to rows based on the selected options
-        const filtered = rows.filter((row) => {
-            const date = dayjs(row.Date, { format: 'DD/MM/YYYY' }); // Parse date using dayjs
-            const fromDateMatch = !fromDate || date.isSameOrAfter(fromDate, 'day');
-            const toDateMatch = !toDate || date.isBefore(dayjs(toDate).add(1, 'day'), 'day');
-            const branchMatch = !branch || row.Branch.toLowerCase() === branch.toLowerCase();
-            const typeMatch = !type || row.Type.toLowerCase() === type.toLowerCase();
-            const statusMatch = !status || row.Status.toLowerCase() === status.toLowerCase();
-
-            return fromDateMatch && toDateMatch && branchMatch && typeMatch && statusMatch;
-        });
-
-        setFilteredRows(filtered);
-    }, [fromDate, toDate, branch, type, status]);
+        const result = isDateInRange(currentDate, startDate, endDate);
 
 
     const handleDelete = (id) => {
@@ -130,22 +112,59 @@ export default function AccessibleTable() {
                 setFilteredRows(updatedRows);
             }
         })
-        // Filter out the row with the specified ID
-
-
     };
+
+    const dateFormater = (day) => {
+		let date
+		if (day == null) {
+			date = new Date('');
+            return null;
+		} else {
+			date = new Date(day)
+		}
+		let month = date.getMonth()
+		month++;
+
+		if (month < 10) {
+			month = '0' + month;
+		}
+		let dayy = date.getDate()
+
+		if (dayy < 10) {
+			dayy = '0' + dayy
+		}
+		return date.getFullYear() + '-' + month + '-' + dayy
+	}
+
+    useEffect(() => {
+        const filtered = rows.filter((row) => {
+            const fromDateMatch = !fromDate || dateFormater(row.Date) >= fromDate ;
+            const toDateMatch = !toDate || dateFormater(row.Date) <= toDate
+            const branchMatch = !branch || row.Branch.toLowerCase() === branch.toLowerCase();
+            const typeMatch = !type || row.Type.toLowerCase() === type.toLowerCase();
+            const statusMatch = !status || row.Status.toLowerCase() === status.toLowerCase();
+
+            return fromDateMatch && toDateMatch && branchMatch && typeMatch && statusMatch;
+        });
+
+        setFilteredRows(filtered);
+    }, [fromDate, toDate, branch, type, status]);
 
     return (
         <>
             <div className='main'>
-                <h3 className='text-center'>Admin panel to view the data of transactions paid by the customer.</h3>
+            <h3 className='text-center'>Admin panel to view the data of transactions paid by the customer.</h3>
+
                 <div className='filer-container my-5'>
                     <div className='row'>
                         <div className='col-auto'>
                             <div>
                                 <LocalizationProvider dateAdapter={AdapterDayjs} fullWidth>
                                     <DemoContainer components={['DatePicker']}>
-                                        <DatePicker label="Form" />
+                                        <DatePicker label="Form"
+                                            value={fromDate}
+                                            onChange={(date) => handleDateChange(date, 'from')}
+                                        />
                                     </DemoContainer>
                                 </LocalizationProvider>
                             </div>
@@ -178,13 +197,13 @@ export default function AccessibleTable() {
                                             <em>All</em>
                                         </MenuItem>
                                         <MenuItem value={'Thane'}>Thane</MenuItem>
-                                        <MenuItem value={'navi mumbai'}>navi mumbai</MenuItem>
-                                        <MenuItem value={'mumbai'}>mumbai</MenuItem>
-                                        <MenuItem value={'kurla'}>kurla</MenuItem>
-                                        <MenuItem value={'vile parle'}>vile parle</MenuItem>
-                                        <MenuItem value={'lower parel'}>lower parel</MenuItem>
-                                        <MenuItem value={'andheri'}>andheri</MenuItem>
-                                        <MenuItem value={'byculla'}>byculla</MenuItem>
+                                        <MenuItem value={'Navi mumbai'}>Mavi mumbai</MenuItem>
+                                        <MenuItem value={'Mumbai'}>Mumbai</MenuItem>
+                                        <MenuItem value={'Kurla'}>Kurla</MenuItem>
+                                        <MenuItem value={'Vile parle'}>Vile parle</MenuItem>
+                                        <MenuItem value={'Lower parel'}>Lower parel</MenuItem>
+                                        <MenuItem value={'Andheri'}>Andheri</MenuItem>
+                                        <MenuItem value={'Byculla'}>Byculla</MenuItem>
                                     </Select>
                                 </FormControl>
                             </div>
@@ -249,34 +268,31 @@ export default function AccessibleTable() {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {filteredRows
-                                .sort((a, b) => { 
-                                    const dateA = new Date(a.Date.split('/').reverse().join('/'));
-                                    const dateB = new Date(b.Date.split('/').reverse().join('/')); 
-                                    return dateA - dateB;  
-                                })
-                                .map((row) => (
-                                    <TableRow 
-                                        key={row.ID}>
-                                        <TableCell component="th" scope="row">
-                                            {row.ID}
-                                        </TableCell>
-                                        <TableCell align="left">{row.Date}</TableCell>
-                                        <TableCell align="left">{row.Branch}</TableCell>
-                                        <TableCell align="left">{row.Type}</TableCell>
-                                        <TableCell align="left">{row.Amount}</TableCell>
-                                        <TableCell align="left">{row.Bank}</TableCell>
-                                        <TableCell align="left">{row.Requested}</TableCell>
-                                        <TableCell align="left">{row.Status}</TableCell>
-                                        <TableCell align="left"><HighlightOffOutlinedIcon className='currsore-pointer' onClick={() => handleDelete(row.ID)} /></TableCell>
-                                    </TableRow>
-                                ))} 
+                            {filteredRows.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row.ID}
+                                    </TableCell>
+                                    <TableCell align="left">{row.Date}</TableCell>
+                                    <TableCell align="left">{row.Branch}</TableCell>
+                                    <TableCell align="left">{row.Type}</TableCell>
+                                    <TableCell align="left">{row.Amount}</TableCell>
+                                    <TableCell align="left">{row.Bank}</TableCell>
+                                    <TableCell align="left">{row.Requested}</TableCell>
+                                    <TableCell align="left">{row.Status}</TableCell>
+                                    <TableCell align="left">
+                                        <Grid item xs={8}>
+                                            <HighlightOffIcon className='currsore-pointer' onClick={() => handleDelete(row.ID)} />
+                                        </Grid>
+                                    </TableCell>
+                                    
+                                </TableRow>
+                            ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-
             </div>
         </>
-
     );
 }
+export default AdminPanel;
